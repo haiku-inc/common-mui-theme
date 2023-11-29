bd77f05bea3385c82be91fcf5421196c87ef8226
Constantine Nazárenko <const@nazarenko.es>
2023-11-29 20:38:03 +0100

    update packages

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import theme from './theme';
+import darkTheme from './darkTheme';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
 root.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={darkTheme}>
     <App />
   </ThemeProvider>,
 );
